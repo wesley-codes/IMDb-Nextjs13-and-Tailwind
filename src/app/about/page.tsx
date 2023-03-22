d67e1244f45f0d04d5c63f3d688a8adcea44a4dc
wesley-codes <ezemikey12@gmail.com>
2023-03-22 16:09:42 +0100

    added about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600 ">About </h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, est
+        aliquam aspernatur minima inventore aliquid tempora itaque laboriosam
+        dolorum nostrum corporis fugiat vitae? Explicabo earum labore
+        reprehenderit, consequuntur temporibus quis. Voluptatum, sed quisquam
+        facere
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis quasi
+        laborum ipsa nostrum tempore eveniet iure dolore ipsam nulla facere sit
+        obcaecati labore, sunt molestias eius id molestiae minima. Dolores.
+        Necessitatibus eius doloribus, recusandae beatae ab, inventore debitis
+        ea eos repudiandae obcaecati dignissimos numquam alias? Rem omnis labore
+        corporis obcaecati magni iste. Repudiandae officia dolores atque eos?
+        Ipsa, mollitia error!
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis quasi
+        laborum ipsa nostrum tempore eveniet iure dolore ipsam nulla facere sit
+        obcaecati labore, sunt molestias eius id molestiae minima. Dolores.
+        Necessitatibus eius doloribus, recusandae beatae ab, inventore debitis
+        ea eos repudiandae obcaecati dignissimos numquam alias? Rem omnis labore
+        corporis obcaecati magni iste. Repudiandae officia dolores atque eos?
+        Ipsa, mollitia error!
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis quasi
+        laborum ipsa nostrum tempore eveniet iure dolore ipsam nulla facere sit
+        obcaecati labore, sunt molestias eius id molestiae minima. Dolores.
+        Necessitatibus eius doloribus, recusandae beatae ab, inventore debitis
+        ea eos repudiandae obcaecati dignissimos numquam alias? Rem omnis labore
+        corporis obcaecati magni iste. Repudiandae officia dolores atque eos?
+        Ipsa, mollitia error!
+      </p>
+    </div>
+  );
+}
